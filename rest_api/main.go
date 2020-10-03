52e1996bfe56483537e79b6d47d950ba7cdd785c
Kamaldeep Singh <kamal@Kamaldeeps-MBP.attlocal.net>
2020-10-03 00:14:53 -0500

    added getevents,  updateevent and create event

--- rest_api/main.go
+++ rest_api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -25,9 +26,57 @@ var event1 = event{
 var events = allEvents{event1}
 
 func getEvents(w http.ResponseWriter, r *http.Request) {
-	eventsByte, err := json.Marshal(events)
+	err := json.NewEncoder(w).Encode(events)
 	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+	}
+}
 
+func errWriter(w http.ResponseWriter, statusCode int, errMsg string) {
+	w.WriteHeader(statusCode)
+	w.Write([]byte(errMsg))
+}
+
+func createEvent(w http.ResponseWriter, r *http.Request) {
+	var newEvent event
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		errWriter(w, http.StatusInternalServerError, err.Error())
+	}
+	err = json.Unmarshal(reqBody, &newEvent)
+	if err != nil {
+		errWriter(w, http.StatusInternalServerError, err.Error())
+	}
+	events = append(events, newEvent)
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newEvent)
+}
+
+func updateEvent(w http.ResponseWriter, r *http.Request) {
+	var updatedEvent event
+	id := mux.Vars(r)["id"]
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		errWriter(w, http.StatusBadRequest, err.Error())
+	}
+	err = json.Unmarshal(reqBody, &updatedEvent)
+	if err != nil {
+		errWriter(w, http.StatusBadRequest, err.Error())
+	}
+	eventFound := false
+	for _, e := range events {
+		if e.ID == id {
+			e.Description = updatedEvent.Description
+			e.Title = updatedEvent.Title
+			eventFound = true
+		}
+	}
+	if !eventFound {
+		errWriter(w, http.StatusBadRequest, "Event ID not found")
+	} else {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(updatedEvent)
 	}
 }
 
@@ -38,5 +87,8 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", homePage)
+	router.HandleFunc("/getevents", getEvents)
+	router.HandleFunc("/createvent", createEvent)
+	router.HandleFunc("/updateEvent", updateEvent)
 	http.ListenAndServe(":8181", router)
 }
