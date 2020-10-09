f2067d648ca16150e4784bb5137aa8f3588d7a0e
Kamaldeep Singh <kamal@Kamaldeeps-MBP.attlocal.net>
2020-10-09 11:26:28 -0500

    update

--- sherlockandAnagram/main.go
+++ sherlockandAnagram/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"sort"
 	"strings"
 )
 
 func sherlockAndAnagrams(s string) int32 {
-	stringArr := strings.Split(s, ",")
-	for i:=0;i<len(s);i++ {
-		for j:=1;i<len(s)
+	stringMap := make(map[string]int)
+	stringArr := strings.Split(s, "")
+	for i := 0; i < len(stringArr); i++ {
+		stringMap[stringArr[i]]++
+		for j := i + 2; j < len(stringArr)+1; j++ {
+			b := strings.Split(s, "")
+			a := b[i:j]
+			sort.Strings(a)
+			sortedString := strings.Join(a, "")
+			stringMap[sortedString]++
+		}
 	}
-	sort.Strings(stringArr[:1])
+	sum := 0
+	for _, v := range stringMap {
+		sum += (v * (v - 1)) / 2
+	}
+	return int32(sum)
+}
+
+func main() {
+	fmt.Println(sherlockAndAnagrams("kkkk"))
 }
